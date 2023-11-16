3feff42083aab43f8236203382c587b62c7663e9
jennraven1 <jenniffer.paderes@ravenstack.com>
2023-11-16 22:08:16 +0800

    updated related to smoke test

--- RECRPRO/Resources/Elements/CommonModule.py
+++
@@ -1,3 +0,0 @@
-AUTH_USER = 'id:nav-user-name'
-BROWSER_MODE = 'browser'
-EMPTY_REQUIRED_FIELD = 'id:this_field_is_required'
--- RECRPRO/custom-library/environment.py
+++
@@ -1,6 +0,0 @@
-def getBaseURL(env='sit'):
-    if env == 'sit':
-        url = 'https://secretproject1.com'
-    elif env == 'prod':
-        url = 'https://ravenrecruit.com'
-    return url
